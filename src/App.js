238592334963e8c876543aaf2632a0ea65e92785
Gonzalo <gonzalo.rubino@gmail.com>
2021-09-04 21:20:10 -0300

    Cleanup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+document.title = "Search on Github";
+
 const App = () => (
   <div>
     <Header />
--- src/SearchShow.js
+++ src/SearchShow.js
@@ -1,57 +1,58 @@
-import React, { useState, useEffect} from "react";
-import { useLocation } from "react-router-dom";
+import React from "react";
 import { Octokit } from "@octokit/core";
 
 import "./css/SearchShow.css";
 import Loading from "./components/Loading";
 import ResultRow from "./components/ResultRow";
 
-const SearchShow = ({history}) => {
-  const location = useLocation();
-  const useMountEffect = (fun) => useEffect(fun, [fun]);
-  const octokit = new Octokit({
-    auth: process.env.NEXT_PUBLIC_GITHUB_KEY,
-  });
-  const [search, setSearch] = useState();
 
-  function itemList(){
-    let items =[];
+const octokit = new Octokit({
+  auth: process.env.NEXT_PUBLIC_GITHUB_KEY,
+});
+class SearchShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { search: null };
+  }
 
-    if(search && search.data){
-     items = search.data.items.map((item,idx) => (
-       <ResultRow key={idx} item={item} />
-     ));
+  componentDidMount() {
+    this.octoDataReq(this.props.location.search);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      this.setState({ search: null });
+      this.octoDataReq(this.props.location.search);
     }
-    
+  }
+
+  itemList() {
+    let items = [];
+
+    if (this.state.search && this.state.search.data) {
+      items = this.state.search.data.items.map((item, idx) => (
+        <ResultRow key={idx} item={item} />
+      ));
+    }
+
     return items;
   }
 
-  async function octoDataReq(val) {
+  async octoDataReq(val) {
     const githubSearch = await octokit.request(`GET /search/repositories`, {
-      q: val || location.search,
+      q: val,
     });
 
-    setSearch(githubSearch);
-  };
+    this.setState({ search: githubSearch });
+  }
 
-  useMountEffect(octoDataReq);
-  
-  history.listen((change) => {
-    if (change.search !== location.search){
-      setSearch(null);
-      octoDataReq(change.search);
-    }
-  });
-
-  return (
-    <div className="search-input row mt-5">
-      {search ? (
-        itemList()
-      ) : (
-        <Loading />
-      )}
-    </div>
-  );
+  render() {
+    return (
+      <div className="search-input row mt-5">
+        {this.state.search && this.state.search ? this.itemList() : <Loading />}
+      </div>
+    );
+  }
 };
 
 /**
